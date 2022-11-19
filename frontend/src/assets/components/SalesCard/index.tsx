a2fb77142f07aa970aa67e7cba578c8af4b2dde6
GuilhermeSergio <gui.sergiiio@gmail.com>
2022-11-19 11:25:11 -0300

    send notification

--- frontend/src/assets/components/SalesCard/index.tsx
+++ frontend/src/assets/components/SalesCard/index.tsx
@@ -78,7 +78,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationActionButton />
+                                            <NotificationActionButton saleId={sale.id}/>
                                         </div>
                                     </td>
                                 </tr>
